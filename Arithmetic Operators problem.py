f0319f5bedc2b4918d6cf34ca32db957e3d33242
Coders Haze <pentester.mdkafiulhasan@gmail.com>
2025-04-23 12:43:54 -0400

    Arithmetic Operators problem.py
    There are all Arithmetic Operators-related problems, and solutions there.

---
+++ Arithmetic Operators problem.py
@@ -0,0 +1,33 @@
+#Write a program to input 2 Numbers & Print their Sum
+
+first = int(input("enter first:"))
+second = int(input("enter second:"))
+
+print("sum =",first + second)
+
+#Write a program to input 2 Numbers & Print their difference
+first = int(input("enter first:"))
+second = int(input("enter second:"))
+
+print("diff =",first - second)
+
+#Write a program to input 2 Numbers & Print their Multiplication
+
+first = int(input("enter first:"))
+second = int(input("enter second:"))
+
+print("multi =",first * second)
+
+#Write a program to input 2 Numbers & Print their Division
+
+first = int(input("enter first:"))
+second = int(input("enter second:"))
+
+print("div =",first / second)
+
+#Write a program to input 2 Numbers & Print their Mod
+
+first = int(input("enter first:"))
+second = int(input("enter second:"))
+
+print("mod =",first % second)
